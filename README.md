c4a5949a9c54200abb565e2f76a88ba7f79917bc
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-08 14:13:48 +0330

    add: postgresQL deps

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <h1 align="center">
-SQL Kafka Debezium
+    SQL Kafka Debezium
 </h1>
 
 Using Debezium to publish PostgresQL data over Kafka.
---
+++ internal/database/database.go
@@ -0,0 +1,13 @@
+package database
+
+import (
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "127.0.0.1"
+	port     = 5432
+	user     = "postgres"
+	password = "super-secret-password"
+	dbname   = "henry-db"
+)
