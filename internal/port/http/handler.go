4ce5a7a6406358afbbb93880c1db4d71e4b319f6
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-13 09:01:45 +0330

    feat: handle post requests

--- internal/port/http/handler.go
+++ internal/port/http/handler.go
@@ -1,11 +1,30 @@
 package http
 
-import "database/sql"
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+const (
+	insertQuery = "INSERT INTO `users` (`name`, `email`) VALUES (?, ?)"
+)
 
 type Handler struct {
 	Connection *sql.DB
 }
 
-func (h *Handler) HandleRequests() {
+func (h *Handler) HandleRequests(ctx *fiber.Ctx) error {
+	var userReq request
 
+	if err := ctx.BodyParser(&userReq); err != nil {
+		return ctx.SendStatus(http.StatusBadRequest)
+	}
+
+	if _, err := h.Connection.Query(insertQuery, userReq.name, userReq.email); err != nil {
+		return ctx.SendStatus(http.StatusInternalServerError)
+	}
+
+	return ctx.SendString("user added")
 }
---
+++ internal/port/http/request.go
@@ -0,0 +1,6 @@
+package http
+
+type request struct {
+	name  string `json:"name"`
+	email string `json:"email"`
+}
