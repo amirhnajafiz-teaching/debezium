526aaded8efc3effa6b6632300e966f30099d24f
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-15 10:02:07 +0330

    add: interface mehtods

--- connector/internal/kafka/kafka.go
+++ connector/internal/kafka/kafka.go
@@ -1,16 +1,26 @@
 package kafka
 
 import (
-	"context"
-
 	"github.com/segmentio/kafka-go"
 )
 
 type Client interface {
-	OpenConnection(ctx context.Context, url string) error
+	OpenConnection(url string) error
 	Consume() error
 }
 
 type client struct {
 	conn *kafka.Conn
+}
+
+func NewClient() Client {
+	return &client{}
+}
+
+func (c *client) OpenConnection(url string) error {
+	return nil
+}
+
+func (c *client) Consume() error {
+	return nil
 }
