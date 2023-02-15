9f6defe2705eb3c68873c32ae40a905ec59988d9
amirhnajafiz <najafizadeh21@gmail.com>
2023-02-15 10:08:39 +0330

    add: main connector

--- connector/main.go
+++ connector/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
 
+	"github.com/amirhnajafiz/debezium/connector/internal/kafka"
+)
+
+func main() {
+	var (
+		KafkaURL       = flag.String("url", "", "kafka cluster address")
+		KafkaTopic     = flag.String("topic", "", "kafka topic")
+		KafkaPartition = flag.Int("partition", 0, "kafka partition")
+	)
+
+	// parsing flags
+	flag.Parse()
+
+	// create a new kafka client
+	c := kafka.NewClient()
+	if er := c.OpenConnection(*KafkaURL, *KafkaTopic, *KafkaPartition); er != nil {
+		panic(er)
+	}
+
+	log.Println("start consuming ...")
+
+	// start consuming
+	if er := c.Consume(); er != nil {
+		panic(er)
+	}
 }
